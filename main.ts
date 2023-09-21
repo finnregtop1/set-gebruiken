b24578c026f94d59fd9dc094229de85d88b97fa5
finnregtop1 <140831629+finnregtop1@users.noreply.github.com>
2023-09-21 16:13:04 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let licht = 0
+basic.forever(function () {
+    licht = pins.analogReadPin(AnalogPin.P0)
+    if (licht > 200) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # .
+            . # # # .
+            # # # . .
+            `)
+    }
+})
